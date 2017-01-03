893cefc5872caf6179812be93c160e17331ce40d
Daher Alfawares <daher.alfawares@live.com>
2017-01-02 20:41:14 -0600

    access to cards and other types

--- Board/WhiteboardInfo.swift
+++ Board/WhiteboardInfo.swift
@@ -9,18 +9,24 @@
 import Foundation
 
 public struct WhiteboardInfo : Info {
-    private var boardItems : [String]
     private var boardTitle :  String
+    private var boardItems : [String]
+    private var boardCards : [String]
+    private var boardTexts  : [String]
     
     public init(with dictionary:[String:AnyObject]){
-        self.boardItems = dictionary["items"] as? [String] ?? []
         self.boardTitle = dictionary["title"] as? String ?? ""
+        self.boardItems = dictionary["items"] as? [String] ?? []
+        self.boardCards = dictionary["cards"] as? [String] ?? []
+        self.boardTexts = dictionary["texts"] as? [String] ?? []
     }
     public func encoded() -> [String:AnyObject] {
         return
         [
             "title" : boardTitle as AnyObject,
-            "items" : boardItems as AnyObject
+            "items" : boardItems as AnyObject,
+            "cards" : boardCards as AnyObject,
+            "texts" : boardTexts as AnyObject
         ]
     }
     public mutating func items() -> [String] {
@@ -34,4 +40,14 @@ public struct WhiteboardInfo : Info {
             boardItems.remove(at: index)
         }
     }
+    
+    
+    public func cards() -> [String] {
+        return boardCards
+    }
+    
+    public func texts() -> [String] {
+        return boardTexts
+    }
+    
 }
