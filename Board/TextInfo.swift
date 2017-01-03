f4fdb87f693a168c0f4626f27c93da2f5dfd9598
Daher Alfawares <daher.alfawares@live.com>
2017-01-02 20:05:26 -0600

    Adding another type

---
+++ Board/TextInfo.swift
@@ -0,0 +1,28 @@
+//
+//  Card.swift
+//  Board
+//
+//  Created by Daher Alfawares on 11/5/16.
+//  Copyright © 2016 Daher Alfawares. All rights reserved.
+//
+
+import Foundation
+
+
+public struct TextInfo : Info {
+    public var t = ""
+    public var l = "Text"
+    
+    public init(with value:[String:AnyObject]){
+        t = String( value["t"] as? String ?? "" ) ?? ""
+        l = String( value["l"] as? String ?? "" ) ?? ""
+    }
+    
+    public func encoded() -> [String:AnyObject] {
+        return [
+            "t": t as AnyObject,
+            "l": l as AnyObject
+        ]
+    }
+}
+
