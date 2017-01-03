1175e70409e39c25bd403429371027d0ba5452c4
Daher Alfawares <daher.alfawares@live.com>
2017-01-02 21:34:03 -0600

    Board adds text

--- Board/WhiteboardInfo.swift
+++ Board/WhiteboardInfo.swift
@@ -11,42 +11,39 @@ import Foundation
 public struct WhiteboardInfo : Info {
     private var boardTitle :  String
     private var boardItems : [String]
-    private var boardCards : [String]
-    private var boardTexts  : [String]
+    private var boardTypes : [String:String]
     
     public init(with dictionary:[String:AnyObject]){
         self.boardTitle = dictionary["title"] as? String ?? ""
         self.boardItems = dictionary["items"] as? [String] ?? []
-        self.boardCards = dictionary["cards"] as? [String] ?? []
-        self.boardTexts = dictionary["texts"] as? [String] ?? []
+        self.boardTypes = dictionary["types"] as? [String:String] ?? [:]
     }
     public func encoded() -> [String:AnyObject] {
         return [
             "title" : boardTitle as AnyObject,
             "items" : boardItems as AnyObject,
-            "cards" : boardCards as AnyObject,
-            "texts" : boardTexts as AnyObject
+            "types" : boardTypes as AnyObject,
         ]
     }
     public mutating func items() -> [String] {
         return boardItems
     }
-    public mutating func appendItem(with id:String){
-        self.boardItems.append(id)
+    public mutating func types() -> [String:String] {
+        return boardTypes
     }
+    public mutating func appendItem(with id:String, type: String){
+        self.boardItems.append(id)
+        self.boardTypes[id] = type
+    }
+    
     public mutating func removeItem(with id:String){
-        if let index = boardItems.index(of: id) {
+        if let index = boardItems.index(of: id){
             boardItems.remove(at: index)
         }
+        
+        if let index = boardTypes.index(forKey: id) {
+            boardTypes.remove(at: index)
+        }
     }
-    
-    
-    public func cards() -> [String] {
-        return boardCards
-    }
-    
-    public func texts() -> [String] {
-        return boardTexts
-    }
-    
 }
+
