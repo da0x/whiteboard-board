a069c019309c8fd4e43a86a64d3a31aabc4038ea
Daher Alfawares <daher.alfawares@live.com>
2017-02-21 06:25:09 -0600

    Added archive to board info

--- Board/UserInfo.swift
+++ Board/UserInfo.swift
@@ -9,28 +9,27 @@
 import Foundation
 
 public struct UserInfo : Info {
-    public enum UserType : String {
-        case local  = "local"
-        case google = "google"
-    }
     
     private var name        : String
-    private var type        : UserType
+    private var type        : String
     private var boardList   : [String]
-    private var titles : [String:String]
+    private var archiveList : [String]
+    private var titles      : [String:String]
     
     public init(with dictionary:[String:AnyObject]){
         self.name          = dictionary["name"] as? String ?? ""
         self.boardList     = dictionary["boards"] as? [String] ?? []
-        self.type          = UserType(rawValue: dictionary["type"] as? String ?? "local")!
+        self.archiveList   = dictionary["archive"] as? [String] ?? []
+        self.type          = dictionary["type"] as? String ?? "local"
         self.titles        = dictionary["titles"] as? [String:String] ?? [:]
     }
     public func encoded() -> [String:AnyObject] {
         return [
-            "name"   : name as AnyObject,
-            "boards" : boardList as AnyObject,
-            "type"   : type.rawValue as AnyObject,
-            "titles" : titles as AnyObject,
+            "name"    : name as AnyObject,
+            "boards"  : boardList as AnyObject,
+            "archive" : archiveList as AnyObject,
+            "type"    : type as AnyObject,
+            "titles"  : titles as AnyObject,
         ]
     }
     public mutating func boards() -> [String] {
@@ -42,6 +41,10 @@ public struct UserInfo : Info {
     public mutating func appendItem(with id:String, titled title:String){
         self.boardList.append(id)
         self.titles[id] = title
+    }
+    public mutating func archiveItem(with id: String){
+        self.removeItem(with: id)
+        self.archiveList.append(id)
     }
     public mutating func merge(boards:[String],titles:[String:String]){
         let set1 = Set(boardList)
@@ -63,14 +66,14 @@ public struct UserInfo : Info {
     public mutating func set(name: String){
         self.name = name
     }
-    public mutating func set(type: UserType){
+    public mutating func set(type: String){
         self.type = type
     }
     public mutating func set(title: String, with id: String){
         self.titles[id] = title
     }
     
-    public func userType() -> UserType { return self.type }
+    public func userType() -> String { return self.type }
     public func userName() -> String { return self.name }
 }
 
